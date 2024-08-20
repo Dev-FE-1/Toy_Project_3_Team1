aef81df687b4057130457ff769969ca3c3534a83
Gin Song <sjgaru@gmail.com>
2024-08-20 17:24:59 +0900

    Initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'prettier',
+    'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  parser: '@typescript-eslint/parser',
+  plugins: ['react', 'prettier', '@typescript-eslint'],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    'no-console': 'warn',
+    'no-unused-vars': 'warn',
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+  },
+}
