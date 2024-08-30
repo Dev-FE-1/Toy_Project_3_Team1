ba6088e63654e196b6fe3c231d7277e7cfb92996
seoyoonyi <yiseoyoon@gmail.com>
2024-08-31 00:25:50 +0900

    ✨ #38 message 상수값으로 추가

---
+++ src/constants/messages.ts
@@ -0,0 +1,9 @@
+export const MESSAGES = {
+  LOGIN: {
+    FAIL: '아이디 또는 비밀번호를 입력해주세요.',
+  },
+  RESET_PASSWORD: {
+    SUCCESS: '비밀번호 재설정 링크가 이메일로 전송되었습니다.',
+    FAIL: '비밀번호 재설정 링크 전송에 실패했습니다. 다시 시도해주세요.',
+  },
+}
