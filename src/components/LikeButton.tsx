19b45b213ff2a5c2cd6fe88798354b821428ab4f
Gin Song <sjgaru@gmail.com>
2024-09-02 03:54:57 +0900

    ☠️  #59 좋아요 프롭스, 타입에러 수정

--- src/components/LikeButton.tsx
+++ src/components/LikeButton.tsx
@@ -2,18 +2,23 @@ import { Heart } from 'lucide-react'
 import { useLikeButton } from '@/api/playlist/likePlaylist'
 import styled from '@emotion/styled'
 
+type StyledHeartProps = {
+  isLiked: boolean
+}
+type StyledCountProps = {
+  isLiked: boolean
+}
+
 const LikeButton = ({ playlistId }: { playlistId: string }) => {
   const { isLiked, likeCount, toggleLike } = useLikeButton(playlistId)
 
   return (
-    <>
-      <Container>
-        <StyledHeart className="like-button" onClick={toggleLike} isLiked={isLiked} />
-        <StyledCount className="like-count" isLiked={isLiked}>
-          {likeCount}
-        </StyledCount>
-      </Container>
-    </>
+    <Container>
+      <StyledHeart className="like-button" isLiked={isLiked} onClick={toggleLike} />
+      <StyledCount isLiked={isLiked} className="like-count">
+        {likeCount}
+      </StyledCount>
+    </Container>
   )
 }
 
@@ -28,12 +33,16 @@ const Container = styled.div`
   }
   .like-count {
     font-size: 0.8em;
+    display: inline-block;
   }
 `
 
-const StyledHeart = styled(Heart)`
-  color: ${(props) => (props.isLiked ? 'red' : 'initial')};
+const StyledHeart = styled(Heart, {
+  shouldForwardProp: (prop) => prop !== 'isLiked',
+})<StyledHeartProps>`
+  color: ${(props) => (props.isLiked ? 'red' : 'gray')};
 `
-const StyledCount = styled.div`
-  color: ${(props) => (props.isLiked ? 'red' : 'initial')};
+
+const StyledCount = styled.div<StyledCountProps>`
+  color: ${(props) => (props.isLiked ? 'red' : 'gray')};
 `
