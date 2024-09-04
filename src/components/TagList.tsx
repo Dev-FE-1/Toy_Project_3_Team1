4cd5ae8772b6bbcba2d277e0ce2c1d74b22c2033
jizero <jizero27@gmail.com>
2024-09-04 18:11:25 +0900

    📝 #86 플리추가 페이지 디자인 및 밑줄입력 컴포넌트 생성, 리팩토링

---
+++ src/components/TagList.tsx
@@ -0,0 +1,59 @@
+import styled from '@emotion/styled'
+import { colors } from '@/constants/color'
+import { fontSize } from '@/constants/font'
+import { X } from 'lucide-react'
+
+interface TagListProps {
+  tags: string[]
+  onTagDelete: (id: number) => void
+}
+
+const TagList = ({ tags, onTagDelete }: TagListProps) => {
+  return (
+    <Container>
+      <div className="container-tag">
+        {tags.map((t, idx) => (
+          <span key={idx} className="tag">
+            {t}
+            <div className="btn-tagdelete" onClick={() => onTagDelete(idx)}>
+              <X size={18} />
+            </div>
+          </span>
+        ))}
+      </div>
+    </Container>
+  )
+}
+
+export default TagList
+
+const Container = styled.div`
+  .tag {
+    background-color: ${colors.lightPurPle};
+    color: ${colors.primaryPurple};
+  }
+
+  .btn-tagdelete {
+    border: none !important;
+    cursor: pointer;
+    margin-top: 4px;
+    margin-left: 2px;
+  }
+
+  .container-tag {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 8px;
+    max-width: 100%;
+    white-space: nowrap;
+  }
+
+  .tag {
+    border-radius: 20px;
+    padding: 5px 10px;
+    font-size: ${fontSize.sm};
+    display: flex;
+    align-items: center;
+    flex-shrink: 0;
+  }
+`
--- src/pages/CreatePlaylistPage.tsx
+++ src/pages/CreatePlaylistPage.tsx
@@ -1,30 +1,30 @@
-import React, { useState, KeyboardEvent } from 'react'
+import React, { useState } from 'react'
 import styled from '@emotion/styled'
-import { X } from 'lucide-react'
 import { colors } from '@/constants/color'
 import { fontSize } from '@/constants/font'
 import { createPlayList } from '@/api/playlist/createPlayList'
-import { videoListProps } from '@/types/playlistType'
 import Button from '@/components/common/Button/Button'
-import Input from '@/components/common/Input/Input'
-import { Trash2 } from 'lucide-react'
 import PLlogo from '@/assets/createlist_logo.svg'
+import LineInput from '@/components/common/Input/LineInput'
+import TagList from '@/components/TagList'
+import { MESSAGES } from '@/constants/messages'
+import useTags from '@/hooks/useTags'
+import usePLItem from '@/hooks/usePLItem'
+import MusicItem from '@/components/MusicItem'
 
 const CreatePlaylistPage = () => {
   const [title, setTitle] = useState('')
-  const [currentTag, setCurrentTag] = useState('')
-  const [tag, setTag] = useState<string[]>([])
-  const [url, setUrl] = useState('')
-  const [isValid, setIsValid] = useState(true)
-  const [videoList, setVideoList] = useState<videoListProps[]>([])
   const [isPrivate, setIsPrivate] = useState(false)
+  const { tags, currentTag, setCurrentTag, isTagValid, handleAddTag, handleDeleteTag, setTags } =
+    useTags()
+  const { url, setVideoList, videoList, handleAddList, isValid, setUrl, handleDelete } = usePLItem()
 
   const handleUploadMusic = async (e: React.FormEvent) => {
     e.preventDefault()
     try {
-      await createPlayList(title, tag, videoList, isPrivate)
+      await createPlayList(title, tags, videoList, isPrivate)
       setTitle('')
-      setTag([])
+      setTags([])
       setUrl('')
       setVideoList([])
       setCurrentTag('')
@@ -34,115 +34,28 @@ const CreatePlaylistPage = () => {
     }
   }
 
-  const handleAddList = async () => {
-    const isValidYoutubeUrl = (url: string) => {
-      const regex = /^(https?:\/\/)?(www\.youtube\.com|youtu\.?be)\/.+$/
-      return regex.test(url)
-    }
-
-    if (!isValidYoutubeUrl(url)) {
-      setIsValid(false)
-      return
-    }
-
-    setIsValid(true)
-
-    const youtubeKey = import.meta.env.VITE_YOUTUBE_API_KEY
-    const videoId = url.split('v=')[1]?.split('&')[0]
-    const videoUrl = `https://www.googleapis.com/youtube/v3/videos?part=snippet&id=${videoId}&key=${youtubeKey}`
-
-    try {
-      const res = await fetch(videoUrl)
-      const data = await res.json()
-
-      if (data.items && data.items.length > 0) {
-        const videoData = {
-          title: data.items[0].snippet.title,
-          channelTitle: data.items[0].snippet.channelTitle,
-          url: `https://www.youtube.com/watch?v=${videoId}`,
-          thumbnail: data.items[0].snippet.thumbnails.medium.url,
-        }
-        setVideoList((prev: videoListProps[]) => [...prev, videoData])
-        setUrl('')
-      }
-    } catch (error) {
-      console.error('add list error :', error)
-    }
-  }
-
-  const handleTag = (e: KeyboardEvent) => {
-    if (e.key === 'Enter' && currentTag.trim() !== '' && e.nativeEvent.isComposing === false) {
-      e.preventDefault()
-      const saveTag = currentTag.trim().startsWith('#')
-        ? currentTag.trim()
-        : `#${currentTag.trim()}`
-      setTag((prev: string[]) => [...prev, saveTag])
-      setCurrentTag('')
-    }
-  }
-
-  const handleDelete = (id: number) => {
-    setVideoList((prev: videoListProps[]) => prev.filter((_, index) => index !== id))
-  }
-
-  const handleTagDelete = (id: number) => {
-    setTag((prev) => prev.filter((_, index) => index !== id))
-  }
-
   const handlePrivate = () => {
     setIsPrivate(!isPrivate)
   }
 
-  const isUploadDisabled = title.trim() === '' || tag.length === 0 || videoList.length === 0
+  const isUploadDisabled = title.trim() === '' || tags.length === 0 || videoList.length === 0
 
   return (
     <Container>
       <form onSubmit={handleUploadMusic}>
         <div className="section-input">
-          <Input
+          <LineInput
             className="input-title"
             type="text"
+            lineType="thin"
             placeholder="플레이리스트의 제목을 입력하세요."
             value={title}
             onChange={(e) => setTitle(e.target.value)}
           />
-          <Input
-            className="input-tag"
-            type="text"
-            placeholder="태그를 입력하세요."
-            value={currentTag}
-            onChange={(e) => setCurrentTag(e.target.value)}
-            onKeyDown={handleTag}
-          />
-          <div className="container-tag">
-            {tag.map((t, idx) => (
-              <span key={idx} className="tag">
-                {t}
-                <button
-                  className="btn-tagdelete"
-                  type="button"
-                  onClick={() => handleTagDelete(idx)}
-                >
-                  <X size={18} />
-                </button>
-              </span>
-            ))}
-          </div>
         </div>
         {videoList.length > 0 ? (
           <div className="list-music">
-            {videoList.map((item: videoListProps, idx) => (
-              <div key={idx} className="item-video">
-                <img src={item.thumbnail} alt={`${item.title}`} />
-                <div>
-                  <p>{item.title}</p>
-                  <span>{item.channelTitle}</span>
-                </div>
-                <button className="btn-delete" type="button" onClick={() => handleDelete(idx)}>
-                  <Trash2 color={colors.lightGray} />
-                </button>
-              </div>
-            ))}
+            <MusicItem videoList={videoList} onItemDelete={handleDelete} />
           </div>
         ) : (
           <div className="list-music">
@@ -156,24 +69,38 @@ const CreatePlaylistPage = () => {
             </div>
           </div>
         )}
+        <div className="input-youtube">
+          <LineInput
+            className="input-link"
+            type="text"
+            lineType="thin"
+            placeholder="유튜브 링크를 입력하세요."
+            value={url}
+            onChange={(e) => setUrl(e.target.value)}
+          />
+          <button className="btn-add" type="button" onClick={handleAddList}>
+            곡 추가
+          </button>
+        </div>
+        {!isValid && <span className="text-warning">{MESSAGES.CREATE_PL.YOUTUBE}</span>}
         <div className="section-upload">
-          <div className="check-private">
+          <LineInput
+            className="input-tag"
+            type="text"
+            lineType="none"
+            placeholder="태그를 입력하세요."
+            value={currentTag}
+            onChange={(e) => setCurrentTag(e.target.value)}
+            onKeyDown={handleAddTag}
+          />
+          <TagList tags={tags} onTagDelete={handleDeleteTag} />
+        </div>
+        {!isTagValid && <span className="text-warning">{MESSAGES.CREATE_PL.TAG_FAIL}</span>}
+        <div className="pl-upload">
+          <label className="check-private">
             <input type="checkbox" onChange={handlePrivate} checked={isPrivate} />
-            비공개
-          </div>
-          <div className="input-youtube">
-            <Input
-              className="input-link"
-              type="text"
-              placeholder="유튜브 링크를 입력하세요."
-              value={url}
-              onChange={(e) => setUrl(e.target.value)}
-            />
-            <button className="btn-add" type="button" onClick={handleAddList}>
-              곡 추가
-            </button>
-          </div>
-          {!isValid && <span className="text-warning">유튜브 링크를 입력해주세요.</span>}
+            <span>비공개로 업로드</span>
+          </label>
           <Button disabled={isUploadDisabled}>등록하기</Button>
         </div>
       </form>
@@ -186,17 +113,27 @@ export default CreatePlaylistPage
 const Container = styled.div`
   display: flex;
   flex-direction: column;
+  height: 80vh;
 
-  .section-input,
-  .list-music {
-    margin: 0 20px;
-  }
-
-  .section-upload {
+  .pl-upload {
     position: fixed;
     bottom: 55px;
     width: 100%;
     max-width: 430px;
+  }
+
+  .list-music,
+  .input-youtube,
+  .section-upload,
+  .text-warning {
+    margin: 0 20px;
+  }
+
+  .section-input {
+    margin: 10px 20px 0;
+  }
+
+  .section-upload {
     align-items: center;
   }
 
@@ -214,7 +151,6 @@ const Container = styled.div`
   .btn_add {
     width: 100%;
     padding: 16px 18px;
-    margin-bottom: 10px;
     border: 1px solid ${colors.lightGray};
     border-radius: 5px;
   }
@@ -222,7 +158,7 @@ const Container = styled.div`
   .list-music {
     padding: 20px 0;
     display: flex;
-    height: 350px;
+    height: 380px;
     flex-direction: column;
     gap: 16px;
     overflow-y: auto;
@@ -240,42 +176,6 @@ const Container = styled.div`
       height: 73px;
       margin: 60px 0 26px 0;
     }
-  }
-
-  .item-video {
-    display: flex;
-    align-items: center;
-    padding: 10px;
-    border-radius: 5px;
-    background-color: ${colors.white};
-  }
-
-  .item-video img {
-    width: 120px;
-    height: 90px;
-    object-fit: cover;
-    border-radius: 5px;
-    margin-right: 14px;
-  }
-
-  .item-video div {
-    display: flex;
-    flex-direction: column;
-    width: 210px;
-    margin-right: 14px;
-  }
-
-  .item-video p {
-    font-size: ${fontSize.md};
-    font-weight: bold;
-    margin: 0;
-    color: ${colors.black};
-  }
-
-  .item-video span {
-    font-size: ${fontSize.sm};
-    color: ${colors.gray};
-    margin-top: 4px;
   }
 
   .input-youtube {
@@ -304,51 +204,31 @@ const Container = styled.div`
     cursor: pointer;
   }
 
-  .btn-upload {
-    border: 1px solid ${colors.lightGray};
-    border-radius: 5px;
-    background-color: ${colors.primaryPurple};
-    color: ${colors.white};
-    cursor: pointer;
-  }
-
-  .btn-upload:disabled {
-    pointer-events: none;
-    opacity: 0.4;
-  }
-
   .text-warning {
     color: red;
-    margin-left: 10px;
     font-size: ${fontSize.sm};
   }
 
-  .container-tag {
-    display: flex;
-    flex-wrap: nowrap;
-    gap: 8px;
-    max-width: 100%;
-    overflow-x: auto;
-    white-space: nowrap;
-  }
-
-  .tag {
-    background-color: ${colors.primaryPurple};
-    border-radius: 20px;
-    padding: 5px 10px;
-    font-size: ${fontSize.sm};
-    color: ${colors.white};
+  .check-private {
     display: flex;
     align-items: center;
-    flex-shrink: 0;
-  }
+    margin-bottom: 5px;
+    margin-left: 10px;
 
-  .btn-tagdelete {
-    background-color: ${colors.primaryPurple};
-    border: none;
-    color: ${colors.white};
-    cursor: pointer;
-    margin-top: 3px;
-    margin-left: 2px;
+    input {
+      appearance: none;
+      margin-right: 10px;
+      width: 20px;
+      height: 20px;
+      border: 2px solid ${colors.lightGray};
+      border-radius: 4px;
+      cursor: pointer;
+      position: relative;
+    }
+
+    input:checked {
+      background-color: ${colors.primaryPurple};
+      border-color: ${colors.black};
+    }
   }
 `
