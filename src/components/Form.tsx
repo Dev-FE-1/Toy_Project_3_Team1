9f25cc03df29a68e491e995920c00bbfb833dd52
Gin Song <sjgaru@gmail.com>
2024-09-06 02:11:25 +0900

    ✨ #47 회원가입 페이지 수정

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,51 +1,104 @@
-import { FormInputs, FormProps } from '@/types/formType'
+import React from 'react'
+import styled from '@emotion/styled'
 import { useForm } from 'react-hook-form'
+import Input from '@/components/common/Input/Input'
+import Button from '@/components/common/Button/Button'
+import { colors } from '@/constants/color'
+import { fontSize } from '@/constants/font'
 
-const Form: React.FC<FormProps> = ({ title, getDataForm, firebaseError }) => {
+export interface FormInputs {
+  email: string
+  password: string
+  confirmPassword?: string
+}
+
+interface FormProps {
+  title: string
+  onSubmit: (data: FormInputs) => void
+  firebaseError?: string
+  includeConfirmPassword?: boolean
+}
+
+const Form: React.FC<FormProps> = ({
+  title,
+  onSubmit,
+  firebaseError,
+  includeConfirmPassword = false,
+}) => {
   const {
     register,
     handleSubmit,
     formState: { errors },
-    reset,
+    watch,
   } = useForm<FormInputs>({
     mode: 'onChange',
   })
 
-  const onSubmit = ({ email, password }: FormInputs) => {
-    getDataForm({ email, password })
-    reset()
-  }
-
-  const userEmail = {
-    required: '이메일을 입력해주세요.',
-    pattern: {
-      value: /\S+@\S+\.\S+/,
-      message: '이메일 형식이 올바르지 않습니다.',
-    },
-  }
-
-  const userPassword = {
-    required: '비밀번호를 입력해주세요.',
-    minLength: {
-      value: 6,
-      message: '비밀번호는 6자 이상이어야 합니다.',
-    },
-  }
+  const password = watch('password')
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <div>
-        <input type="email" placeholder="E-mail" {...register('email', userEmail)} />
-        {errors.email && <p>{errors.email.message}</p>}
-      </div>
-      <div>
-        <input type="password" placeholder="password" {...register('password', userPassword)} />
-        {errors.password && <p>{errors.password.message}</p>}
-      </div>
-      <button type="submit">{title}</button>
-      {firebaseError && <p>{firebaseError}</p>}
-    </form>
+    <FormContainer onSubmit={handleSubmit(onSubmit)}>
+      <Input
+        type="text"
+        placeholder="이메일을 입력해주세요."
+        value={watch('email') || ''}
+        {...register('email', {
+          required: '이메일을 입력해주세요.',
+          pattern: {
+            value: /\S+@\S+\.\S+/,
+            message: '이메일 형식이 올바르지 않습니다.',
+          },
+        })}
+      />
+      {errors.email && <span className="error-notice">{errors.email.message}</span>}
+
+      <Input
+        type="password"
+        placeholder="비밀번호를 입력해주세요."
+        value={watch('password') || ''}
+        {...register('password', {
+          required: '비밀번호를 입력해주세요.',
+          minLength: {
+            value: 6,
+            message: '비밀번호는 6자 이상이어야 합니다.',
+          },
+        })}
+      />
+      {errors.password && <span className="error-notice">{errors.password.message}</span>}
+
+      {includeConfirmPassword && (
+        <>
+          <Input
+            type="password"
+            placeholder="비밀번호를 다시 입력해주세요."
+            value={watch('confirmPassword') || ''}
+            {...register('confirmPassword', {
+              required: '비밀번호를 다시 입력해주세요.',
+              validate: (value) => value === password || '비밀번호가 일치하지 않습니다.',
+            })}
+          />
+          {errors.confirmPassword && (
+            <span className="error-notice">{errors.confirmPassword.message}</span>
+          )}
+        </>
+      )}
+
+      {firebaseError && <span className="error-notice">{firebaseError}</span>}
+      <Button onClick={handleSubmit(onSubmit)}>{title}</Button>
+    </FormContainer>
   )
 }
 
 export default Form
+
+const FormContainer = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  width: 100%;
+
+  .error-notice {
+    color: ${colors.red};
+    font-size: ${fontSize.sm};
+  }
+`
--- src/pages/SignUpPage.tsx
+++ src/pages/SignUpPage.tsx
@@ -1,16 +1,20 @@
 import { useState } from 'react'
-import Form from '@/components/Form'
+import styled from '@emotion/styled'
 import { useNavigate } from 'react-router-dom'
 import { app } from '@/firebase/firebaseConfig'
 import { createUserWithEmailAndPassword, getAuth } from 'firebase/auth'
-import { FormInputs } from '@/types/formType'
+import Form from '@/components/Form'
+import ButtonLink from '@/components/common/Button/ButtonLink'
+import { PATH } from '@/constants/path'
+import { fontSize, fontWeight } from '@/constants/font'
+import logo from '@/assets/myidoru_logo.svg'
 
-const SignUp = () => {
+const SignUpPage = () => {
   const navigate = useNavigate()
   const [firebaseError, setFirebaseError] = useState('')
-
   const auth = getAuth(app)
-  const handleSignupAndLogin = ({ email, password }: FormInputs) => {
+
+  const handleSignUp = ({ email, password }: { email: string; password: string }) => {
     createUserWithEmailAndPassword(auth, email, password)
       .then(() => {
         navigate('/')
@@ -22,8 +26,39 @@ const SignUp = () => {
   }
 
   return (
-    <Form title={'회원가입'} getDataForm={handleSignupAndLogin} firebaseError={firebaseError} />
+    <Container>
+      <div className="logo-container">
+        <img src={logo} alt="logo-myidoru" />
+      </div>
+      <Form
+        title="회원가입"
+        onSubmit={handleSignUp}
+        firebaseError={firebaseError}
+        includeConfirmPassword={true}
+      />
+
+      <ButtonLink variant="text" to={PATH.LOGIN}>
+        이미 계정이 있으신가요? 로그인하기
+      </ButtonLink>
+    </Container>
   )
 }
 
-export default SignUp
+export default SignUpPage
+
+const Container = styled.div`
+  padding: 62px 20px;
+
+  .logo-container {
+    margin-bottom: 40px;
+    display: flex;
+    justify-content: center;
+  }
+
+  h1 {
+    text-align: center;
+    margin-bottom: 20px;
+    font-size: ${fontSize.xl};
+    font-weight: ${fontWeight.bold};
+  }
+`
