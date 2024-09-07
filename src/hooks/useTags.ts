54ae04064580a2003d94187eead6121defd19b5d
seoyoonyi <yiseoyoon@gmail.com>
2024-09-07 10:38:11 +0900

    ☠️ #85 태그 공백시 에러처리

--- src/hooks/useTags.ts
+++ src/hooks/useTags.ts
@@ -1,19 +1,29 @@
 import { useState, KeyboardEvent } from 'react'
+import { MESSAGES } from '@/constants/messages'
 
-const MAX_TAG_COUNT = 6
-const MAX_TAG_LENGTH = 7
+export const MAX_TAG_COUNT = 6
+export const MAX_TAG_LENGTH = 7
 
 const useTags = () => {
   const [tags, setTags] = useState<string[]>([])
   const [currentTag, setCurrentTag] = useState('')
   const [isTagValid, setIsTagValid] = useState(true)
+  const [errorMessage, setErrorMessage] = useState('')
 
   const handleAddTag = (e: KeyboardEvent) => {
     if (e.key === 'Enter' && currentTag.trim() !== '' && e.nativeEvent.isComposing === false) {
       e.preventDefault()
 
+      if (currentTag.includes(' ')) {
+        setIsTagValid(false)
+        setErrorMessage(MESSAGES.CREATE_PL.TAG_SPACE_FAIL)
+        setCurrentTag('')
+        return
+      }
+
       if (tags.length >= MAX_TAG_COUNT) {
         setIsTagValid(false)
+        setErrorMessage(MESSAGES.CREATE_PL.TAG_COUNT_FAIL)
         setCurrentTag('')
         return
       }
@@ -24,11 +34,13 @@ const useTags = () => {
 
       if (saveTag.length > MAX_TAG_LENGTH) {
         setIsTagValid(false)
+        setErrorMessage(MESSAGES.CREATE_PL.TAG_LENGTH_FAIL)
         setCurrentTag('')
         return
       }
 
       setIsTagValid(true)
+      setErrorMessage('')
       setTags((prev: string[]) => [...prev, saveTag])
       setCurrentTag('')
     }
@@ -43,6 +55,7 @@ const useTags = () => {
     currentTag,
     setCurrentTag,
     isTagValid,
+    errorMessage,
     handleAddTag,
     handleDeleteTag,
     setTags,
