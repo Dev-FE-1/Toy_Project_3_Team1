9fc37fe0332717793b9cb3b5bf05229cf750cb45
seoyoonyi <yiseoyoon@gmail.com>
2024-09-09 05:56:22 +0900

    🔨 #85 플레이리스트 관련 api 및 타입정의 수정

--- src/service/playlist/getFollowingUsersPlaylists.ts
+++ src/service/playlist/getFollowingUsersPlaylists.ts
@@ -1,40 +1,46 @@
-import { db, auth } from '@/firebase/firebaseConfig'
+import { auth, db } from '@/firebase/firebaseConfig'
 import { doc, getDoc } from 'firebase/firestore'
 import getUserPlaylists from '@/service/playlist/getUserPlaylists'
+import { FollowedUserPlaylists } from '@/types/playlistType'
 
-// 현재 로그인된 사용자가 팔로우한 사용자들의 플레이리스트를 가져오는 함수
-const getFollowingUsersPlaylists = async () => {
-  try {
-    const currentUser = auth.currentUser
-    if (!currentUser) {
-      throw new Error('User is not logged in')
-    }
+// 현재 로그인된 사용자가 팔로우한 사용자들의 플레이리스트를 한 번에 모두 가져오는 함수
+const getFollowingUsersPlaylists = async (): Promise<FollowedUserPlaylists[]> => {
+  return new Promise((resolve, reject) => {
+    const unsubscribe = auth.onAuthStateChanged(async (user) => {
+      if (user) {
+        try {
+          const uid = user.uid
+          const userRef = doc(db, 'USERS', uid)
+          const userSnap = await getDoc(userRef)
 
-    const uid = currentUser.uid
+          if (!userSnap.exists()) {
+            console.error('User not found')
+            resolve([])
+            return
+          }
 
-    const userRef = doc(db, 'USERS', uid)
-    const userSnap = await getDoc(userRef)
+          const userData = userSnap.data()
+          const followingUserIds = userData.following || []
 
-    if (!userSnap.exists()) {
-      console.error('User not found')
-      return []
-    }
+          const followingPlaylists: FollowedUserPlaylists[] = await Promise.all(
+            followingUserIds.map(async (userId: string) => {
+              const userPlaylists = await getUserPlaylists(userId)
+              return { userId, playlists: userPlaylists }
+            })
+          )
 
-    const userData = userSnap.data()
-    const followingUserIds = userData.following || []
-
-    const followingPlaylists = await Promise.all(
-      followingUserIds.map(async (userId: string) => {
-        const userPlaylists = await getUserPlaylists(userId)
-        return { userId, playlists: userPlaylists }
-      })
-    )
-
-    return followingPlaylists
-  } catch (error) {
-    console.error('Error fetching followed users playlists:', error)
-    return []
-  }
+          resolve(followingPlaylists)
+        } catch (error) {
+          console.error('Error fetching followed users playlists:', error)
+          reject(error)
+        }
+      } else {
+        console.error('User is not logged in')
+        reject(new Error('User is not logged in'))
+      }
+      unsubscribe()
+    })
+  })
 }
 
 export default getFollowingUsersPlaylists
